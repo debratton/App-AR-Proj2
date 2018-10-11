d5eff588280068e4260f6add28c76ff0d4283ac2
debratton <debratton@gmail.com>
2018-10-11 10:44:54 -0500

    Start

---
+++ ObjectFun/ObjectFun/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ObjectFun
+//
+//  Created by David E Bratton on 10/11/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
