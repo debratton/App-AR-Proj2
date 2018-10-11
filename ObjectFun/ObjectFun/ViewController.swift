ea2aa90a34d31fed404ee7a2e38b6fa3b0378985
debratton <debratton@gmail.com>
2018-10-11 17:27:39 -0500

    Done

--- ObjectFun/ObjectFun/ViewController.swift
+++ ObjectFun/ObjectFun/ViewController.swift
@@ -17,6 +17,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        sceneView.automaticallyUpdatesLighting = true
+        
+        let cube = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue
+        cube.materials = [material]
+        
+        let cubeNode = SCNNode()
+        cubeNode.geometry = cube
+        cubeNode.position = SCNVector3(0.0, 0.0, -1.0)
+        
+        let rotation = SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 2)
+        let repeatRotation = SCNAction.repeatForever(rotation)
+        cubeNode.runAction(repeatRotation)
+        
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
